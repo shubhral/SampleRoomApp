02c3493d62cea2c0935cb000a270497bc6bf88b6
shubhral <ykumar.shubhral@gmail.com>
2018-03-25 20:52:56 +0530

    updated libraries & initialized few things in application

--- app/src/main/java/com/shubhral/myfirstmvvp/MyApp.java
+++ app/src/main/java/com/shubhral/myfirstmvvp/MyApp.java
@@ -4,11 +4,14 @@ import android.app.Application;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import timber.log.Timber;
 
 /**
  * Created by Shubhral on 3/25/2018.
@@ -20,13 +23,22 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
 
+        Timber.plant(new Timber.DebugTree());
         Gson gson = new GsonBuilder().create();
 
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor()
-                .setLevel(HttpLoggingInterceptor.Level.BASIC);
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                Timber.i(message);
+            }
+        }).setLevel(HttpLoggingInterceptor.Level.BASIC);
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .addInterceptor(interceptor)
+                .build();
+
+        Picasso picasso = new Picasso.Builder(this)
+                .downloader(new OkHttp3Downloader(okHttpClient))
                 .build();
 
         Retrofit retrofit = new Retrofit.Builder()
